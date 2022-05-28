1950480a2dbaf5cfed5b856f49fe1c764539d50d
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:11:48 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs16.js
@@ -0,0 +1,7 @@
+function tugas16(){
+    const array = [45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10];
+    const array2 = [45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5];
+    console.log(Math.min(...array));
+    console.log(Math.max(...array2));
+}
+tugas16();
